9540e657ad51d77f1681e395029edbf61baa0a82
nok2015 <standbyme20@hotmail.co.jp>
2015-10-07 07:13:50 +0000

    add want/have btn

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ActiveRecord::Base
   serialize :raw_info , Hash
 
-  has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
+  has_many :ownerships, foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+  # 外部キーとしてitem_idを用いる。wantsは仮想的な中間テーブルとなる
+  has_many :wants, foreign_key: "item_id", dependent: :destroy
+  has_many :want_users, through: :wants, source: :user #belongs_to :userのuserを取得する
+  
+  # 外部キーとしてitem_idを用いる。havesは仮想的な中間テーブルとなる
+  has_many :haves, foreign_key: "item_id", dependent: :destroy
+  has_many :have_users, through: :haves, source: :user
+  
+  
 end
